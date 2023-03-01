3c9dadf9a4f9ec21a695999f7975d301c2c9d22a
Kuuhaku <Kuuhaku@192.168.55.237>
2023-03-01 14:51:13 +0000

    Criar printLeftAndRight

--- src/andre/main.java
+++ src/andre/main.java
@@ -4,18 +4,36 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		printTopOrBottom(5);
-		System.out.println("*   *");
-		System.out.println("*   *");
-		System.out.println("*   *");
-		printTopOrBottom(5);
+		printTopOrBottom(4);
+		printLeftAndRight(4,2);
+		printTopOrBottom(4);
 	}
 
 	public static void printTopOrBottom(int size) {
-		for (int i = 1; i < size; i++) {
+		
+		for (int i = 0; i < size; i++) {
 			
 			System.out.print("*");
 		}
+		System.out.println("");
+	}
+	
+	public static void printLeftAndRight(int width, int height) {
 		
+		for (int i = 0; i < height; i++) {
+			
+			System.out.print("*");
+			
+			for (int j = 0; j < width - 2; j++) {
+				
+				System.out.print(" ");
+			}
+			
+			System.out.println("*");
+			
+		}
+		
+		
+
 	}
 }
