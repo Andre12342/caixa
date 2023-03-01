7aac4bbc6afeb5824d0df98a591264d15576cb13
Kuuhaku <Kuuhaku@192.168.55.237>
2023-03-01 14:39:56 +0000

    Paramereterizar printTopOrBottom

--- src/andre/main.java
+++ src/andre/main.java
@@ -4,15 +4,18 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		printTopOrBottom();
+		printTopOrBottom(5);
 		System.out.println("*   *");
 		System.out.println("*   *");
 		System.out.println("*   *");
-		printTopOrBottom();
+		printTopOrBottom(5);
 	}
 
-	public static void printTopOrBottom() {
-		System.out.println("*****");
+	public static void printTopOrBottom(int size) {
+		for (int i = 1; i < size; i++) {
+			
+			System.out.print("*");
+		}
 		
 	}
 }
